aeee30696cab611df29b9798aa3cc57cafd053e9
Nicolas Hahn <nicolas@stonespring.org>
2015-12-12 15:50:38 -0800

    change to relative path

--- write_stance_tweets_csv.sh
+++ write_stance_tweets_csv.sh
@@ -1,1 +1,1 @@
-python3 /home/nick/TwitterSearchToDatabase/write_stance_tweets_csv.py iac_write 'IAC PASSWORD GOES HERE' localhost/iac
+python3 ./write_stance_tweets_csv.py iac_write 'IAC PASSWORD GOES HERE' localhost/iac
